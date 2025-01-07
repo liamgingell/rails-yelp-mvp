6f0e3b849830951026f5e3ae57fbf481d4ae76ce
Liam Gingell <elgee95@gmail.com>
2025-01-07 21:46:28 +0900

    Created routes for Restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+Review.destroy_all
+
+restaurants = Restaurant.create!([
+  { name: "Sushi Place", address: "123 Sushi St", category: "japanese" },
+  { name: "Pasta House", address: "456 Pasta Rd", category: "italian" },
+  { name: "Baguette Bakery", address: "789 Baguette Blvd", category: "french" }
+])
+
+reviews = Review.create!([
+  { content: "Great sushi!", rating: 5, restaurant: restaurants[0] },
+  { content: "Delicious pasta!", rating: 4, restaurant: restaurants[1] },
+  { content: "Amazing baguettes!", rating: 5, restaurant: restaurants[2] }
+])
