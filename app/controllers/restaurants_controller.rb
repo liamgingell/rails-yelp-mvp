06e0f2f37d211e93077d7d4b5e44896ed37778b7
Liam Gingell <elgee95@gmail.com>
2025-01-08 15:15:47 +0900

    Added a little bit more

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = @restaurant.reviews.build
   end
 
   def new
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,14 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
