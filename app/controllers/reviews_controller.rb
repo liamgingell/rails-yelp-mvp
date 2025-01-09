c8ad49435bc6f7d118b2b5245ed42421e8e61069
Liam Gingell <elgee95@gmail.com>
2025-01-09 18:18:39 +0900

    Added routes to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,17 +9,41 @@ class ReviewsController < ApplicationController
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build(review_params)
+    if @review.save
+      redirect_to @restaurant, notice: 'Review was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @review, notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_url, notice: 'Review was successfully destroyed.'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
